275578b1e5d2275f6ed616d7680f88a57c0b9eeb
Radosław Kot <rdkt13@gmail.com>
2022-03-26 18:02:32 +0100

    add signalr client and disable implict usings

--- dotnet-client/dotnet-client-app/Program.cs
+++ dotnet-client/dotnet-client-app/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
