39af24270a4d404312df5f8c6bcb60462b929372
Radosław Kot <rdkt13@gmail.com>
2022-06-05 13:58:14 +0200

    Introduce HubResponse trait

--- lib/playground/src/example_hub.rs
+++ lib/playground/src/example_hub.rs
@@ -3,10 +3,12 @@ use async_trait;
 use flume::r#async::SendSink;
 use futures::{Sink, SinkExt, Stream, StreamExt};
 use serde;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use signalrs_core::{extensions::StreamExtR, protocol::*};
+use std::ops::Add;
 use std::{any::Any, collections::HashMap, fmt::Debug, sync::Arc};
 use tokio;
+use tokio::sync::oneshot::error;
 use tokio::sync::Mutex;
 
 const WEIRD_ENDING: &'static str = "\u{001E}";
@@ -90,53 +92,35 @@ impl HubInvoker {
                 let target: Target = serde_json::from_str(&text).unwrap();
                 match target.target.as_str() {
                     "non_blocking" => {
-                        self.hub.non_blocking();
+                        Self::text_invocation(&text, |_: EmptyArgs| self.hub.non_blocking(), output)
+                            .await
+                            .unwrap()
                     }
-                    "add" => {
-                        let mut invocation: Invocation<AddArgs> =
-                            serde_json::from_str(&text).unwrap();
-
-                        let arguments = invocation.arguments().unwrap();
-
-                        let result = self.hub.add(arguments.0, arguments.1);
-
-                        match invocation.id() {
-                            Some(id) => result.forward(id.clone(), output).await.unwrap(),
-                            None => {}
-                        }
-                    }
-                    "single_result_failure" => {
-                        let mut invocation: Invocation<SingleResultFailureArgs> =
-                            serde_json::from_str(&text).unwrap();
-
-                        let arguments = invocation.arguments().unwrap();
-
-                        let result = self.hub.single_result_failure(arguments.0, arguments.1);
-
-                        match (invocation.id(), result) {
-                            (Some(id), value) => {
-                                value.forward(id.clone(), output).await.unwrap();
-                            }
-                            _ => {}
-                        }
-                    }
-                    "batched" => {
-                        let mut invocation: Invocation<BatchedArgs> =
-                            serde_json::from_str(&text).unwrap();
-
-                        let arguments = invocation.arguments().unwrap();
-
-                        let result = self.hub.batched(arguments.0);
-
-                        match invocation.id() {
-                            Some(id) => {
-                                result.forward(id.clone(), output).await.unwrap();
-                            }
-                            None => {}
-                        }
-                    }
+                    "add" => Self::text_invocation(
+                        &text,
+                        |arguments: AddArgs| self.hub.add(arguments.0, arguments.1),
+                        output,
+                    )
+                    .await
+                    .unwrap(),
+                    "single_result_failure" => Self::text_invocation(
+                        &text,
+                        |arguments: SingleResultFailureArgs| {
+                            self.hub.single_result_failure(arguments.0, arguments.1)
+                        },
+                        output,
+                    )
+                    .await
+                    .unwrap(),
+                    "batched" => Self::text_invocation(
+                        &text,
+                        |arguments: BatchedArgs| self.hub.batched(arguments.0),
+                        output,
+                    )
+                    .await
+                    .unwrap(),
                     "add_stream" => {
-                        let invocation: Invocation<AddStreamArgs> =
+                        let invocation: Invocation<EmptyArgs> =
                             serde_json::from_str(&text).unwrap();
                         let inv_id = invocation.id().clone().unwrap();
                         if let Some(e) = invocation.stream_ids {
@@ -175,80 +159,46 @@ impl HubInvoker {
                             serde_json::from_str(&text).unwrap();
                         let arguments = stream_invocation.arguments.unwrap();
                         let invocation_id = stream_invocation.invocation_id;
-                        let iidc = invocation_id.clone();
 
-                        let result = self.hub.stream(arguments.0);
+                        let result = self
+                            .hub
+                            .stream(arguments.0)
+                            .forward(invocation_id.clone(), output);
 
-                        let responses = result
-                            .zip(futures::stream::repeat(invocation_id.clone()))
-                            .map(|(e, id)| StreamItem::new(id, e))
-                            .map(|si| serde_json::to_string(&si).unwrap() + WEIRD_ENDING)
-                            .chain(futures::stream::once(async {
-                                let completion: Completion<usize> =
-                                    Completion::new(invocation_id, None, None);
-                                serde_json::to_string(&completion).unwrap() + WEIRD_ENDING
-                            }));
-
-                        let mut responses = Box::pin(responses);
                         let invocations = Arc::clone(&self.ongoing_invocations);
 
-                        let iidc2 = iidc.clone();
+                        let iidc2 = invocation_id.clone();
                         let ongoing = tokio::spawn(async move {
-                            while let Some(item) = responses.next().await {
-                                output.send(item).await.unwrap();
-                            }
+                            result.await.unwrap();
                             let mut invocations = invocations.lock().await;
                             (*invocations).remove(&iidc2);
                         });
 
                         let mut guard = self.ongoing_invocations.lock().await;
-                        (*guard).insert(iidc, ongoing);
+                        (*guard).insert(invocation_id, ongoing);
                     }
                     "stream_failure" => {
                         let stream_invocation: StreamInvocation<StreamFailureArgs> =
                             serde_json::from_str(&text).unwrap();
                         let arguments = stream_invocation.arguments.unwrap();
                         let invocation_id = stream_invocation.invocation_id;
-                        let iidc = invocation_id.clone();
 
-                        let result = self.hub.stream_failure(arguments.0);
+                        let result = self
+                            .hub
+                            .stream_failure(arguments.0)
+                            .forward(invocation_id.clone(), output);
 
-                        let responses = result
-                            .take_while_inclusive(|e| e.is_ok())
-                            .zip(futures::stream::repeat(invocation_id.clone()))
-                            .map(|(e, id)| -> Result<StreamItem<usize>, Completion<()>> {
-                                match e {
-                                    Ok(item) => Ok(StreamItem::new(id, item)),
-                                    Err(e) => Err(Completion::<()>::new(id, None, Some(e))),
-                                }
-                            })
-                            .chain_if(
-                                |e| e.is_ok(),
-                                futures::stream::once(async {
-                                    let r: Result<StreamItem<usize>, Completion<()>> =
-                                        Err(Completion::<()>::new(invocation_id, None, None));
-                                    r
-                                }),
-                            )
-                            .map(|e| match e {
-                                Ok(si) => serde_json::to_string(&si).unwrap() + WEIRD_ENDING,
-                                Err(cmp) => serde_json::to_string(&cmp).unwrap() + WEIRD_ENDING,
-                            });
-
-                        let mut responses = Box::pin(responses);
                         let invocations = Arc::clone(&self.ongoing_invocations);
 
-                        let iidc2 = iidc.clone();
+                        let iidc2 = invocation_id.clone();
                         let ongoing = tokio::spawn(async move {
-                            while let Some(item) = responses.next().await {
-                                output.send(item).await.unwrap();
-                            }
+                            result.await.unwrap();
                             let mut invocations = invocations.lock().await;
                             (*invocations).remove(&iidc2);
                         });
 
                         let mut guard = self.ongoing_invocations.lock().await;
-                        (*guard).insert(iidc, ongoing);
+                        (*guard).insert(invocation_id, ongoing);
                     }
                     _ => panic!(),
                 }
@@ -298,6 +248,34 @@ impl HubInvoker {
             MessageType::Other => { /* panik or kalm? */ }
         }
     }
+
+    async fn text_invocation<'de, T, R, F, S>(
+        text: &'de str,
+        hub_function: F,
+        output: S,
+    ) -> Result<(), Box<dyn std::error::Error>>
+    where
+        T: Deserialize<'de>,
+        F: FnOnce(T) -> R,
+        R: HubResponse,
+        S: Sink<String> + Send + 'static + Unpin + Clone,
+        <S as Sink<String>>::Error: Debug + std::error::Error,
+    {
+        let mut invocation: Invocation<T> = serde_json::from_str(text).unwrap();
+
+        let arguments = invocation.arguments().unwrap();
+
+        let result = hub_function(arguments);
+
+        match (invocation.id(), result) {
+            (Some(id), value) => {
+                value.forward(id.clone(), output).await?;
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
 }
 
 #[async_trait::async_trait]
@@ -313,7 +291,56 @@ pub trait HubResponse {
 }
 
 #[async_trait::async_trait]
-impl HubResponse for i32 {
+impl HubResponse for () {
+    async fn forward<T>(
+        self,
+        _invocation_id: String,
+        _sink: T,
+    ) -> Result<(), Box<dyn std::error::Error>>
+    where
+        T: Sink<String> + Send,
+        <T as Sink<String>>::Error: std::error::Error + 'static,
+    {
+        Ok(())
+    }
+}
+
+macro_rules! impl_hub_response {
+    ($($type:ty),+) => {
+    $(
+        #[async_trait::async_trait]
+        impl HubResponse for $type {
+            async fn forward<T>(
+                self,
+                invocation_id: String,
+                sink: T,
+            ) -> Result<(), Box<dyn std::error::Error>>
+            where
+                T: Sink<String> + Send,
+                <T as Sink<String>>::Error: std::error::Error + 'static,
+            {
+                let completion = Completion::new(invocation_id, Some(self), None);
+                Box::pin(sink)
+                    .send(serde_json::to_string(&completion)? + WEIRD_ENDING)
+                    .await
+                    .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+            }
+        }
+    )*
+    };
+}
+
+impl_hub_response!(usize, isize);
+impl_hub_response!(i8, i16, i32, i64, i128);
+impl_hub_response!(u8, u16, u32, u64, u128);
+impl_hub_response!(f32, f64);
+impl_hub_response!(String);
+
+#[async_trait::async_trait]
+impl<R> HubResponse for Vec<R>
+where
+    R: HubResponse + Send + Serialize,
+{
     async fn forward<T>(
         self,
         invocation_id: String,
@@ -324,34 +351,21 @@ impl HubResponse for i32 {
         <T as Sink<String>>::Error: std::error::Error + 'static,
     {
         let completion = Completion::new(invocation_id, Some(self), None);
+
+        let text = serde_json::to_string(&completion)? + WEIRD_ENDING;
+
         Box::pin(sink)
-            .send(serde_json::to_string(&completion)? + WEIRD_ENDING)
+            .send(text)
             .await
             .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
     }
 }
 
 #[async_trait::async_trait]
-impl HubResponse for Vec<usize> {
-    async fn forward<T>(
-        self,
-        invocation_id: String,
-        sink: T,
-    ) -> Result<(), Box<dyn std::error::Error>>
-    where
-        T: Sink<String> + Send,
-        <T as Sink<String>>::Error: std::error::Error + 'static,
-    {
-        let completion = Completion::new(invocation_id, Some(self), None);
-        Box::pin(sink)
-            .send(serde_json::to_string(&completion)? + WEIRD_ENDING)
-            .await
-            .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
-    }
-}
-
-#[async_trait::async_trait]
-impl HubResponse for Result<u32, String> {
+impl<R> HubResponse for Result<R, String>
+where
+    R: HubResponse + Send + Serialize,
+{
     async fn forward<T>(
         self,
         invocation_id: String,
@@ -365,10 +379,129 @@ impl HubResponse for Result<u32, String> {
             Ok(ok) => Completion::new(invocation_id, Some(ok), None),
             Err(err) => Completion::new(invocation_id, None, Some(err)),
         };
+
+        let text = serde_json::to_string(&completion)? + WEIRD_ENDING;
+
         Box::pin(sink)
-            .send(serde_json::to_string(&completion)? + WEIRD_ENDING)
+            .send(text)
             .await
             .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+    }
+}
+
+// TODO: implement for Result and use to allow user to define their own faillible stream
+pub trait Try {}
+
+pub struct HubStream;
+struct InfallibleHubStream<S>(S);
+struct FallibleHubStream<S>(S);
+
+impl HubStream {
+    pub fn infallible<S, I>(stream: S) -> impl HubResponse
+    where
+        S: Stream<Item = I> + Send + 'static,
+        I: Send + Serialize,
+    {
+        InfallibleHubStream(stream)
+    }
+
+    pub fn fallible<S, I>(stream: S) -> impl HubResponse
+    where
+        S: Stream<Item = Result<I, String>> + Send + 'static,
+        I: Send + Serialize,
+    {
+        FallibleHubStream(stream)
+    }
+}
+
+#[async_trait::async_trait]
+impl<S, I> HubResponse for InfallibleHubStream<S>
+where
+    S: Stream<Item = I> + Send,
+    I: Serialize + Send,
+{
+    async fn forward<T>(
+        self,
+        invocation_id: String,
+        sink: T,
+    ) -> Result<(), Box<dyn std::error::Error>>
+    where
+        T: Sink<String> + Send,
+        <T as Sink<String>>::Error: std::error::Error + 'static,
+    {
+        let result = self.0;
+
+        let responses = result
+            .zip(futures::stream::repeat(invocation_id.clone()))
+            .map(|(e, id)| StreamItem::new(id, e))
+            .map(|si| serde_json::to_string(&si).unwrap() + WEIRD_ENDING)
+            .chain(futures::stream::once(async {
+                let completion: Completion<usize> = Completion::new(invocation_id, None, None);
+                serde_json::to_string(&completion).unwrap() + WEIRD_ENDING
+            }));
+
+        let mut responses = Box::pin(responses);
+        let mut sink = Box::pin(sink);
+
+        while let Some(item) = responses.next().await {
+            sink.send(item)
+                .await
+                .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl<S, I> HubResponse for FallibleHubStream<S>
+where
+    S: Stream<Item = Result<I, String>> + Send,
+    I: Serialize + Send,
+{
+    async fn forward<T>(
+        self,
+        invocation_id: String,
+        sink: T,
+    ) -> Result<(), Box<dyn std::error::Error>>
+    where
+        T: Sink<String> + Send,
+        <T as Sink<String>>::Error: std::error::Error + 'static,
+    {
+        let result = self.0;
+
+        let responses = result
+            .take_while_inclusive(|e| e.is_ok())
+            .zip(futures::stream::repeat(invocation_id.clone()))
+            .map(|(e, id)| -> Result<StreamItem<I>, Completion<()>> {
+                match e {
+                    Ok(item) => Ok(StreamItem::new(id, item)),
+                    Err(e) => Err(Completion::<()>::new(id, None, Some(e))),
+                }
+            })
+            .chain_if(
+                |e| e.is_ok(),
+                futures::stream::once(async {
+                    let r: Result<StreamItem<I>, Completion<()>> =
+                        Err(Completion::<()>::new(invocation_id, None, None));
+                    r
+                }),
+            )
+            .map(|e| match e {
+                Ok(si) => serde_json::to_string(&si).unwrap() + WEIRD_ENDING,
+                Err(cmp) => serde_json::to_string(&cmp).unwrap() + WEIRD_ENDING,
+            });
+
+        let mut responses = Box::pin(responses);
+        let mut sink = Box::pin(sink);
+
+        while let Some(item) = responses.next().await {
+            sink.send(item)
+                .await
+                .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -386,7 +519,7 @@ impl Hub {
     }
 
     pub fn single_result_failure(&self, _a: u32, _b: u32) -> impl HubResponse {
-        Err("An error!".to_string())
+        Err::<u32, String>("An error!".to_string())
     }
 
     pub fn batched(&self, count: usize) -> impl HubResponse {
@@ -395,24 +528,23 @@ impl Hub {
             .collect::<Vec<usize>>()
     }
 
-    pub fn stream(&self, count: usize) -> impl Stream<Item = usize> {
-        stream! {
+    pub fn stream(&self, count: usize) -> impl HubResponse {
+        HubStream::infallible(stream! {
             for i in 0..count {
                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
                 yield i;
             }
-        }
+        })
     }
 
-    // TODO: Break the stream on error?
-    pub fn stream_failure(&self, count: usize) -> impl Stream<Item = Result<usize, String>> {
-        stream! {
+    pub fn stream_failure(&self, count: usize) -> impl HubResponse {
+        HubStream::fallible(stream! {
             for i in 0..count {
                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
                 yield Ok(i);
             }
             yield Err("Ran out of data!".to_string())
-        }
+        })
     }
 
     pub async fn add_stream(&self, input: impl Stream<Item = i32>) -> impl HubResponse {
@@ -436,4 +568,4 @@ struct StreamArgs(usize, #[serde(default)] ());
 struct StreamFailureArgs(usize, #[serde(default)] ());
 
 #[derive(Deserialize, Clone, Debug)]
-struct AddStreamArgs(#[serde(default)] (), #[serde(default)] ());
+struct EmptyArgs(#[serde(default)] (), #[serde(default)] ());
