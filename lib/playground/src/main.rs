09fbe10a89276cd5332c2c077fcb9c5b45a273c9
Radosław Kot <rdkt13@gmail.com>
2022-09-18 09:51:36 +0200

    Create signalr-axum crate

--- lib/playground/src/main.rs
+++ lib/playground/src/main.rs
@@ -1,56 +1,16 @@
 // use playground::{example_hub, ws_transport};
 use async_stream::stream;
-use axum::{
-    extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
-        Extension,
-    },
-    http::{Method, StatusCode},
-    response::{IntoResponse, Json},
-    routing::{get, get_service, post},
-    Router,
-};
-use futures::{select, sink::SinkExt, stream::StreamExt, FutureExt, Stream};
+use axum::{body::Body, http::StatusCode, routing::get_service, Router};
+use futures::{stream::StreamExt, Stream};
 use log::*;
-use signalrs::{
-    connection::ConnectionState,
-    extract::UploadStream,
-    hub::{builder::HubBuilder, Hub},
-    negotiate::{NegotiateResponseV0, TransportSpec},
-    response::ResponseSink,
-};
+use signalrs::{extract::UploadStream, hub::builder::HubBuilder};
+use signalrs_axum::hub_routes;
 use simple_logger::SimpleLogger;
-use std::sync::Arc;
-use tower_http::{
-    cors::{AllowOrigin, CorsLayer},
-    services::fs::ServeFile,
-    trace::TraceLayer,
-};
+use tower_http::{cors::CorsLayer, services::fs::ServeFile, trace::TraceLayer};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // tracing_subscriber::fmt::init();
-
-    SimpleLogger::new()
-        .with_colors(true)
-        .with_local_timestamps()
-        .with_module_level("mio", LevelFilter::Warn)
-        .with_module_level("tokio_tungstenite", LevelFilter::Warn)
-        .with_module_level("tungstenite", LevelFilter::Warn)
-        .with_module_level("hyper", LevelFilter::Warn)
-        .with_module_level("tracing", LevelFilter::Warn)
-        .with_module_level("tower_http", LevelFilter::Warn)
-        .init()
-        .unwrap();
-
-    let cors = CorsLayer::new()
-        .allow_headers(vec![
-            "x-requested-with".parse()?,
-            "x-signalr-user-agent".parse()?,
-        ])
-        .allow_credentials(true)
-        .allow_methods(vec![Method::POST])
-        .allow_origin(AllowOrigin::exact("http://localhost:8080".parse()?));
+    setup_logger();
 
     let index = get_service(ServeFile::new(
         "/home/radoslaw/Programowanie/signalrs/web/index.html",
@@ -62,21 +22,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
     });
 
-    let hub_builder = HubBuilder::new()
-        .method("add", add)
-        .method("add_stream", add_stream)
-        .streaming_method("stream", stream)
-        .streaming_method("stream2", stream2);
-
-    let invoker = Arc::new(hub_builder.build());
-
     let app = Router::new()
         .route("/", index)
-        .route("/chathub/negotiate", post(negotiate).layer(cors))
-        .route(
-            "/chathub",
-            get(ws_upgrade_handler).layer(Extension(invoker)),
-        )
+        .nest("/chathub", build_hub())
+        .layer(CorsLayer::permissive())
         .layer(TraceLayer::new_for_http());
 
     axum::Server::bind(&"0.0.0.0:8080".parse()?)
@@ -86,98 +35,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn negotiate() -> Json<NegotiateResponseV0> {
-    debug!("negotiate endpoint invoked");
+fn build_hub() -> Router<Body> {
+    let hub_builder = HubBuilder::new()
+        .method("add", add)
+        .method("add_stream", add_stream)
+        .streaming_method("stream", stream)
+        .streaming_method("stream2", stream2);
 
-    Json(NegotiateResponseV0 {
-        connection_id: "test".into(),
-        negotiate_version: 0,
-        available_transports: vec![TransportSpec {
-            transport: "WebSockets".into(),
-            transfer_formats: vec!["Text".into()],
-        }],
-    })
-}
-
-async fn ws_upgrade_handler(
-    ws: WebSocketUpgrade,
-    Extension(invoker): Extension<Arc<Hub>>,
-) -> impl IntoResponse {
-    debug!("websocket upgrade request recieved");
-
-    let f = |ws| ws_handler(ws, invoker);
-    ws.on_upgrade(f)
-}
-
-async fn ws_handler(socket: WebSocket, invoker: Arc<Hub>) {
-    let (mut tx_socket, mut rx_socket) = socket.split();
-
-    debug!("performing handshake for a new hub client");
-
-    if let Some(Ok(Message::Text(msg))) = rx_socket.next().await {
-        let response = invoker.handshake(msg.as_str());
-        tx_socket.send(Message::Text(response)).await.unwrap(); // TODO: no unwrap!
-    } else {
-        //TODO: Proper error logging
-        debug!("failed handshake attempt");
-        return;
-    }
-
-    let (tx_channel, irx) = flume::bounded(1000);
-
-    let tx_channel = ResponseSink::new(tx_channel.into_sink());
-    let mut rx_channel = irx.into_stream(); // TODO: verify memory leak bug fixed!
-
-    let connection_state: ConnectionState = Default::default();
-
-    debug!("starting event loop for a new client");
-
-    loop {
-        select! {
-            si = rx_channel.next() => match si {
-                Some(msg) => {
-                    let text = msg.unwrap_text();
-                    debug!("sending to client: {}", text);
-                    tx_socket.send(Message::Text(text)).await.unwrap();
-                },
-                None => {
-                    debug!("rx_channel ended")
-                    /* panik or kalm */
-                },
-            },
-            nm = rx_socket.next().fuse() => match nm {
-                Some(Ok(msg)) => {
-                    debug!("received from client: {:?}", msg);
-                    match msg {
-                        Message::Text(f) => {
-                            let invoker = Arc::clone(&invoker);
-                            let itx = tx_channel.clone();
-                            let csc = connection_state.clone();
-                            tokio::spawn(async move {
-                                if let Err(e) = invoker.invoke_text(f, csc, itx).await {
-                                    error!("error on invoke_text: {}", e);
-                                };
-                            });
-                        }
-                        Message::Binary(_) => {
-                            // do nothing
-                        }
-                        Message::Ping(d) => tx_socket.send(Message::Pong(d)).await.unwrap(),
-                        Message::Pong(_) => { /* ignore */ }
-                        Message::Close(_) => { /* ignore */ }
-                    };
-                }
-                Some(Err(e)) => {
-                    debug!("error while receiving from client: {}", e);
-                }
-                None => {
-                    break;
-                }
-            }
-        }
-    }
-
-    debug!("client event loop stopped");
+    hub_routes(hub_builder.build())
 }
 
 async fn add(a: i32, b: i32) -> i32 {
@@ -209,4 +74,21 @@ pub async fn stream2(count: usize, _input: UploadStream<i32>) -> impl Stream<Ite
             yield i;
         }
     }
+}
+
+fn setup_logger() {
+    // tracing_subscriber::fmt::init();
+
+    SimpleLogger::new()
+        .with_colors(true)
+        .with_local_timestamps()
+        .with_module_level("mio", LevelFilter::Warn)
+        .with_module_level("tokio_tungstenite", LevelFilter::Warn)
+        .with_module_level("tungstenite", LevelFilter::Warn)
+        .with_module_level("hyper", LevelFilter::Warn)
+        .with_module_level("tracing", LevelFilter::Warn)
+        .with_module_level("tower_http", LevelFilter::Warn)
+        .with_utc_timestamps()
+        .init()
+        .unwrap();
 }
---
+++ lib/signalrs-axum/src/lib.rs
@@ -0,0 +1,115 @@
+use axum::{
+    body::Body,
+    extract::ws::*,
+    response::IntoResponse,
+    routing::{get, post},
+    Extension, Json, Router,
+};
+use futures::{future::FutureExt, select, sink::SinkExt, stream::StreamExt};
+use log::*;
+use signalrs::{
+    connection::ConnectionState,
+    hub::Hub,
+    negotiate::{NegotiateResponseV0, TransportSpec},
+    response::ResponseSink,
+};
+use std::sync::Arc;
+
+pub fn hub_routes(hub: Hub) -> Router<Body> {
+    let hub = Arc::new(hub);
+
+    let router = Router::new()
+        .route("/negotiate", post(negotiate))
+        .route("/", get(ws_upgrade_handler).layer(Extension(hub)));
+
+    router
+}
+
+async fn negotiate() -> Json<NegotiateResponseV0> {
+    debug!("negotiate endpoint invoked");
+
+    Json(NegotiateResponseV0 {
+        connection_id: "test".into(),
+        negotiate_version: 0,
+        available_transports: vec![TransportSpec {
+            transport: "WebSockets".into(),
+            transfer_formats: vec!["Text".into()],
+        }],
+    })
+}
+
+async fn ws_upgrade_handler(
+    ws: WebSocketUpgrade,
+    Extension(invoker): Extension<Arc<Hub>>,
+) -> impl IntoResponse {
+    debug!("websocket upgrade request recieved");
+
+    let f = |ws| ws_handler(ws, invoker);
+    ws.on_upgrade(f)
+}
+
+async fn ws_handler(socket: WebSocket, invoker: Arc<Hub>) {
+    let (mut tx_socket, mut rx_socket) = socket.split();
+
+    debug!("performing handshake for a new hub client");
+
+    if let Some(Ok(Message::Text(msg))) = rx_socket.next().await {
+        let response = invoker.handshake(msg.as_str());
+        tx_socket.send(Message::Text(response)).await.unwrap(); // TODO: no unwrap!
+    } else {
+        //TODO: Proper error logging
+        debug!("failed handshake attempt");
+        return;
+    }
+
+    let (tx_channel, irx) = flume::bounded(1000);
+
+    let tx_channel = ResponseSink::new(tx_channel.into_sink());
+    let mut rx_channel = irx.into_stream(); // TODO: verify memory leak bug fixed!
+
+    let connection_state: ConnectionState = Default::default();
+
+    debug!("starting event loop for a new client");
+
+    loop {
+        select! {
+            si = rx_channel.next() => match si {
+                Some(msg) => {
+                    let text = msg.unwrap_text();
+                    debug!("sending to client: {}", text);
+                    tx_socket.send(Message::Text(text)).await.unwrap();
+                },
+                None => {
+                    debug!("rx_channel ended")
+                    /* panik or kalm */
+                },
+            },
+            nm = rx_socket.next().fuse() => match nm {
+                Some(Ok(msg)) => {
+                    debug!("received from client: {:?}", msg);
+                    match msg {
+                        Message::Text(f) => {
+                            if let Err(e) = invoker.invoke_text(f, connection_state.clone(), tx_channel.clone()).await {
+                                error!("error on invoke_text: {}", e);
+                            };
+                        }
+                        Message::Binary(_) => {
+                            // do nothing
+                        }
+                        Message::Ping(d) => tx_socket.send(Message::Pong(d)).await.unwrap(),
+                        Message::Pong(_) => { /* ignore */ }
+                        Message::Close(_) => { /* ignore */ }
+                    };
+                }
+                Some(Err(e)) => {
+                    debug!("error while receiving from client: {}", e);
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+    }
+
+    debug!("client event loop stopped");
+}
