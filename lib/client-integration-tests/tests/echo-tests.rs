d22a1f4c4c25100782aaad1647a639c290e32a5c
Radosław Kot <rdkt13@gmail.com>
2023-04-23 17:56:23 +0200

    Fix minor problems

--- lib/client-integration-tests/tests/echo-tests.rs
+++ lib/client-integration-tests/tests/echo-tests.rs
@@ -2,7 +2,6 @@ use signalrs_client::SignalRClient;
 
 #[tokio::test]
 async fn main() -> anyhow::Result<()> {
-
     let client = SignalRClient::builder("localhost")
         .use_port(5261)
         .use_hub("echo")
